427e53fe4c6236562b5edd88cded21846e1c9fc7
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2024-01-31 23:43:00 +0530

    Update image paths and email address

--- Delicious/forms/book-a-table.php
+++ Delicious/forms/book-a-table.php
@@ -10,7 +10,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'prathamesh13975@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -26,6 +26,13 @@
   $book_a_table->from_email = $_POST['email'];
   $book_a_table->subject = "New table booking request from the website";
 
+  $book_a_table->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
   $book_a_table->smtp = array(
@@ -35,6 +42,7 @@
     'port' => '587'
   );
   */
+  
 
   $book_a_table->add_message( $_POST['name'], 'Name');
   $book_a_table->add_message( $_POST['email'], 'Email');
